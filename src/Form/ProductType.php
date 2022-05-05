1bc94fc3f8a75a2675ae3728f7a211bcec9d6d2f
Kélian <kelian.raoult_severac@edu.devinci.fr>
2022-05-05 14:06:55 +0200

    Add front logic for forms

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -7,9 +7,11 @@ use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
@@ -17,16 +19,38 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, ['required' => true])
-            ->add('description')
-            ->add('size')
-            ->add('keyWords', CollectionType::class, [
-                'entry_type' => TextType::class])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
+                'placeholder' => 'Choose an option',
+                'required' => true
             ])
-        ;
+            ->add('name', TextType::class, ['required' => true])
+            ->add('description', TextType::class, ['required' => true])
+            ->add('size', ChoiceType::class, [
+                'placeholder' => 'Choose a category first',
+                'choices'  => [
+                    'XS' => 'XS',
+                    'S' => 'S',
+                    'M' => 'M',
+                    'L' => 'L',
+                    'XL' => 'XL',
+                    '38' => '38',
+                    '39' => '39',
+                    '40' => '40',
+                    '41' => '41',
+                    '42' => '42',
+                    '43' => '43',
+                    '44' => '44',
+                    '46' => '46',
+                ],
+                'required' => true,
+            ])
+            ->add('keyWords', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                'allow_delete' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
